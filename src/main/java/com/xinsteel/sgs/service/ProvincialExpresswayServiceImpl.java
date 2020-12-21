a76d65b8fd74ff59a44c3923bba9473ecd8bbd9f
“526567097@qq.com” <526567097@qq.com>
2020-12-21 14:54:51 +0800

    初始化项目

--- src/main/java/com/xinsteel/sgs/service/ProvincialExpresswayServiceImpl.java
+++ src/main/java/com/xinsteel/sgs/service/ProvincialExpresswayServiceImpl.java
@@ -1,4 +1,9 @@
 package com.xinsteel.sgs.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component
 public class ProvincialExpresswayServiceImpl {
 }
