d4ab42e60b75d64d09e4fd4483def24ebf4f1fb2
“526567097@qq.com” <526567097@qq.com>
2020-12-23 09:45:17 +0800

    查询订单号接口

--- src/main/java/com/xinsteel/sgs/service/ProvincialExpresswayServiceImpl.java
+++ src/main/java/com/xinsteel/sgs/service/ProvincialExpresswayServiceImpl.java
@@ -5,6 +5,9 @@ import com.xinsteel.sgs.service.impl.ProvincialExpresswayService;
 import com.xinsteel.sgs.utils.HttpRequest;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class ProvincialExpresswayServiceImpl implements ProvincialExpresswayService {
     @Override
@@ -17,15 +20,26 @@ public class ProvincialExpresswayServiceImpl implements ProvincialExpresswayServ
 
     @Override
     public String SelectOrderNo() {
-        String url = "http://172.16.4.238:8080/DataQuerySocket/service/XbToMesPactImportService/SalePlan";
-        String json = "[{\"pageIndex\":1,\"pageSize\":1,\"startTime\":\"2020-08-10\",\"endTime\":\"2020-08-13\",\"memo0\":\"\",\"memo1\":\"\",\"memo2\":\"\",\"memo3\":\"\",\"memo4\": \"95\"}]";
+        String url = "http://172.16.4.238:8080/DataQuerySocket/service/XbToMesPactImportService/SalePlanSq";
+        Map<String,String> data = new HashMap<>();
+        data.put("pageIndex","1");
+        data.put("pageSize","1");
+        data.put("startTime","2020-08-10");
+        data.put("endTime","2020-08-15");
+        data.put("memo0","");
+        data.put("memo1","");
+        data.put("memo2","");
+        data.put("memo3","");
+        data.put("memo4","95");
+
+        String json = "["+ JSONObject.toJSONString(data)+"]";
+        System.out.println(json);
         String result =  HttpRequest.sendPost(url,json);
-        JSONObject jsonObject = JSONObject.parseObject(result);
-        String subJson = jsonObject.getJSONArray("rows").getString(0);
-        JSONObject jsonObject1 = JSONObject.parseObject(subJson);
-        String orderNo = (String) jsonObject1.get("ORDERNO");
+        JSONObject jsonObject1 = JSONObject.parseObject(result);
+        String subJson = jsonObject1.getJSONArray("rows").getString(0);
+        JSONObject jsonObject2 = JSONObject.parseObject(subJson);
+        String orderNo = (String) jsonObject2.get("ORDERNO");
         System.out.println(orderNo);
         return orderNo;
     }
-
 }
